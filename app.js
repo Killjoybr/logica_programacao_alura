4e2f25910de7b063d323075b96f21f6fd236d20c
Ruan David da Silva <112730142+Killjoybr@users.noreply.github.com>
2025-01-17 18:39:40 +0000

    2 modulo desafios

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/*
-TODO: 2 modulo; 3 modulo e 4 modulo
-*/
-
 alert("Boas vindas ao nosso site!");
 
 let nome = "Lua";
@@ -23,3 +19,32 @@ let nomeUsuario = prompt("Qual é seu nome?");
 let idadeUsuario = prompt("Qual é sua idade?");
 
 alert(idadeUsuario >= 18 ? "Pode tirar a habilitação!" : "Não pode tirar a habilitação!");
+
+let fimDeSemana;
+
+let respostaUser = prompt("Qual é o dia da semana?");
+
+respostaUser.toLowerCase() === "sábado" || respostaUser.toLowerCase() === "domingo" ? fimDeSemana = true : fimDeSemana = false;
+
+alert(fimDeSemana ? "Bom fim de semana!" : "Boa semana!");
+
+let numeroUser = prompt("Digite um número positivo ou negativo");
+
+numeroUser < 0 ? alert("Número negativo") : alert("Número positivo ou 0");
+
+if(pontuacao >= 100){
+    alert("Parabéns, você venceu!");
+}; alert("Tente novamente p/ ganhar")
+
+let saldoUser = 2300;
+let nomeUser = "John Doe";
+
+alert(`Olá ${nomeUser}, seu saldo atualmente é de: R$ ${saldoUser}`);
+
+nomeUser = prompt("Digite seu nome:");
+
+alert(`Boas vindas, ${nomeUser}!`)
+
+/*
+TODO: 3 modulo; 4 modulo
+*/
