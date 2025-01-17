bf8c90d9497b75e115561ee76fb040fdc225484f
Ruan David da Silva <112730142+Killjoybr@users.noreply.github.com>
2025-01-17 20:42:42 +0000

    Pending changes exported from your codespace

--- app.js
+++ app.js
@@ -1,50 +1,67 @@
-alert("Boas vindas ao nosso site!");
+// alert("Boas vindas ao nosso site!");
 
-let nome = "Lua";
+// let nome = "Lua";
 
-let idade = 25;
+// let idade = 25;
 
-let numeroDeVendas = 50;
+// let numeroDeVendas = 50;
 
-let saldoDisponivel = 1000;
+// let saldoDisponivel = 1000;
 
-alert("Erro! Preencha todos os campos");
+// alert("Erro! Preencha todos os campos");
 
-let mensagemDeErro = "Erro! Preencha todos os campos";
+// let mensagemDeErro = "Erro! Preencha todos os campos";
 
-alert(mensagemDeErro);
+// alert(mensagemDeErro);
 
-let nomeUsuario = prompt("Qual é seu nome?");
+// let nomeUsuario = prompt("Qual é seu nome?");
 
-let idadeUsuario = prompt("Qual é sua idade?");
+// let idadeUsuario = prompt("Qual é sua idade?");
 
-alert(idadeUsuario >= 18 ? "Pode tirar a habilitação!" : "Não pode tirar a habilitação!");
+// alert(idadeUsuario >= 18 ? "Pode tirar a habilitação!" : "Não pode tirar a habilitação!");
 
-let fimDeSemana;
+// let fimDeSemana;
 
-let respostaUser = prompt("Qual é o dia da semana?");
+// let respostaUser = prompt("Qual é o dia da semana?");
 
-respostaUser.toLowerCase() === "sábado" || respostaUser.toLowerCase() === "domingo" ? fimDeSemana = true : fimDeSemana = false;
+// respostaUser.toLowerCase() === "sábado" || respostaUser.toLowerCase() === "domingo" ? fimDeSemana = true : fimDeSemana = false;
 
-alert(fimDeSemana ? "Bom fim de semana!" : "Boa semana!");
+// alert(fimDeSemana ? "Bom fim de semana!" : "Boa semana!");
 
-let numeroUser = prompt("Digite um número positivo ou negativo");
+// let numeroUser = prompt("Digite um número positivo ou negativo");
 
-numeroUser < 0 ? alert("Número negativo") : alert("Número positivo ou 0");
+// numeroUser < 0 ? alert("Número negativo") : alert("Número positivo ou 0");
 
-if(pontuacao >= 100){
-    alert("Parabéns, você venceu!");
-}; alert("Tente novamente p/ ganhar")
+// if(pontuacao >= 100){
+//     alert("Parabéns, você venceu!");
+// }; alert("Tente novamente p/ ganhar")
 
-let saldoUser = 2300;
-let nomeUser = "John Doe";
+// let saldoUser = 2300;
+// let nomeUser = "John Doe";
 
-alert(`Olá ${nomeUser}, seu saldo atualmente é de: R$ ${saldoUser}`);
+// alert(`Olá ${nomeUser}, seu saldo atualmente é de: R$ ${saldoUser}`);
 
-nomeUser = prompt("Digite seu nome:");
+// nomeUser = prompt("Digite seu nome:");
 
-alert(`Boas vindas, ${nomeUser}!`)
+// alert(`Boas vindas, ${nomeUser}!`)
 
 /*
 TODO: 3 modulo; 4 modulo
 */
+
+// contador 1 ~ 10 
+for(i=1; i<11; i++){
+    console.log(i);
+}
+
+// contador 10 ~ 0
+for(i=10; i>-1; i--){
+    console.log(i);
+}
+
+let contagemUser = prompt("digite um número para sua contagem regressiva");
+contagemUser == undefined ? contagemUser = 0 : contagemUser = parseInt(contagemUser);
+
+for(contagemUser; contagemUser > -1; i--){
+    console.log(contagemUser);
+}
